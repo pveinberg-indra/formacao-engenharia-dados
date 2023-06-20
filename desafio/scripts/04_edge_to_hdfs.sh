bbbf5e0b80229689bf66d7fc686b90c9915d5acb
Pablo Veinberg - Minsait <psebastianv@minsait.com>
2023-06-20 08:26:15 -0300

    Update and fix bugs

--- desafio/scripts/04_edge_to_hdfs.sh
+++ desafio/scripts/04_edge_to_hdfs.sh
@@ -4,13 +4,9 @@
 source <(grep = config.ini)
 
 FROM=${HDFS_LOCAL_DIR}
+RAW=${HDFS_BASE_DIR}${HDFS_LOCAL_DIR}
 
-echo "Iniciando transferência de arquivos de [${FROM}] para HDFS [${HDFS_BASE_DIR}] em ${DATE}..."
-
-echo "Criando filesystem no HDFS..."
-
-docker exec namenode hdfs dfs -mkdir -p ${HDFS_BASE_DIR}/raw
-docker exec namenode hdfs dfs -mkdir -p ${HDFS_BASE_DIR}/gold
+echo "Iniciando transferência de arquivos de [${FROM}] para HDFS [${RAW}] em ${DATE}..."
 
 echo "Estrutura criada em ${HDFS_BASE_DIR}"
 docker exec namenode hdfs dfs -ls ${HDFS_BASE_DIR}
@@ -19,11 +15,11 @@ echo "Copiando os arquivos da borda para o servidor HDFS..."
 for entidade in "${ENTIDADES[@]}"
 do
     echo "Criando diretório da entidade [${entidade}]"
-    docker exec namenode hdfs dfs -mkdir -p ${HDFS_BASE_DIR}/raw/${entidade}
+    docker exec namenode hdfs dfs -mkdir -p ${RAW}/${entidade}
 
     echo "Copiando [${entidade}]"
-    docker exec namenode hdfs dfs -copyFromLocal ${FROM}/${entidade}.csv ${HDFS_BASE_DIR}/raw/${entidade}
+    docker exec namenode hdfs dfs -copyFromLocal ${FROM}/${entidade}.csv ${RAW}/${entidade}
 done
 
-echo "Operação finalizada, arquivos disponíveis em  ${HDFS_BASE_DIR}/raw/"
-docker exec namenode hdfs dfs -ls ${HDFS_BASE_DIR}/raw/
+echo "Operação finalizada, arquivos disponíveis em  ${RAW}/"
+docker exec namenode hdfs dfs -ls ${RAW}
