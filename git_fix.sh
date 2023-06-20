066599b2a55e172efdd2564a84bd96ea8a249446
Pablo Veinberg | Minsait <psebastianv@minsait.com>
2023-06-20 14:50:39 -0300

    Adding git_fix

---
+++ git_fix.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="pveinberg@gmail.com"
+CORRECT_NAME="Pablo Veinberg - Minsait"
+CORRECT_EMAIL="psebastianv@minsait.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
